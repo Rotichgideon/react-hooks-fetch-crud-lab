77eeb193a62e3ad99c1e388d269c2a154a3fb115
gideon rotich <gideonrotich2001@gmail.com>
2024-05-01 00:48:36 +0300

    done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onAddQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,6 +19,42 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    const newQuestion = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex, 10),
+    }
+
+      fetch("http://localhost:4000/questions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newQuestion),
+      })
+      .then((resp) => resp.json())
+      .then((data) => {
+        onAddQuestion(data)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+      setFormData({
+        prompt: "",
+        answer1:"",
+        answer2:"",
+        answer3:"",
+        answer4:"",
+        correctIndex: 0,
+      });
+
     console.log(formData);
   }
 
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((resp) => resp.json())
+    .then((data) => {
+      // console.log(question);
+      setQuestions(data)
+    })
+    .catch((err) => console.error("Err:", err))
+  }, [])
+
+  function handleDeleteQuestion (id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+    .then((resp) => {
+      if (resp.status === 200) {
+        setQuestions((prevQuestions) => 
+          prevQuestions.filter((question) => question.id !== id)
+        )
+      } else {
+        console.error("Failed to delete" + resp.status);
+      }
+    })
+    .catch((err) => {
+      console.error("Err dlt qest:", err);
+    })
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => {
+          // console.log(question);
+          return (
+            <QuestionItem
+            key={question.id} 
+            question={question}
+            onDelete={handleDeleteQuestion} />
+          )
+        })}
+      </ul>
     </section>
   );
 }
